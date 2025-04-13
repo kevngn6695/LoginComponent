571b1f1b8d936187862cdae24ab4ff4c1a2c6a9a
HiepHop <kevngn6695@gmail.com>
2025-04-12 19:45:47 -0700

    First commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+/* Call all needed library */
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+/* Set port number to connect server with client */
+const port = process.env.PORT || 5500;
+
+/* Initialize express js */
+const app = express();
+
+/* Usage the library */
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* Set a path between client and server */
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  // Match all except /api routes
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+/* Listen to initial port */
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
