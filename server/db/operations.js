0495c393f827f8df39802012a684459869977dd3
HiepHop <kevngn6695@gmail.com>
2025-05-02 07:27:07 -0700

    More empty files are added

--- server/db/operations.js
+++ server/db/operations.js
@@ -2,6 +2,25 @@ const { db } = require("./db");
 
 module.exports = {
   // SELECT
+  getAllUsers: async () => {
+    try {
+      const result = await db.query(`SELECT * FROM users`);
+      return result.rows;
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      throw error;
+    }
+  },
+
+  getUserById: async (id) => {
+    try {
+      const result = await db.query(`SELECT * FROM users WHERE id = $1`, [id]);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error fetching user by ID:", error);
+      throw error;
+    }
+  },
 
   // INSERT
   createUser: async (email, password) => {
@@ -18,5 +37,18 @@ module.exports = {
   },
 
   // UPDATE
+  updateUser: async (id, email, password) => {
+    try {
+      const result = await db.query(
+        `UPDATE users SET email = $1, password = $2 WHERE id = $3 RETURNING id, email, password`,
+        [email, password, id],
+      );
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error updating user:", error);
+      throw error;
+    }
+  },
+
   // DELETE
 };
